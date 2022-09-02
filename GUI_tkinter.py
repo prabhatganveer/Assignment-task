584a7f056770dd389132c9efd2b751bb64914650
Prabhat <92990405+prabhatganveer@users.noreply.github.com>
2022-09-02 16:09:02 +0530

    Create GUI_tkinter.py

---
+++ GUI_tkinter.py
@@ -0,0 +1,270 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "stainless-coral",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "'''\n",
+    "1--->how tkinter used in python?\n",
+    "ans->Tkinter is the standard GUI library for Python. Python when combined with Tkinter provides a fast and easy way to create\n",
+    "     GUI applications. Tkinter provides a powerful object-oriented interface to the Tk GUI toolkit. Import the Tkinter module.\n",
+    "'''"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "express-pulse",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "beneficial-debut",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "'''\n",
+    "2--->How do you create a window and frame?\n",
+    "ans->window = Tk(),frame = Frame()\n",
+    "'''"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "black-thailand",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "italic-matthew",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "'''\n",
+    "3--->How do you create an event loop?\n",
+    "ans->window.mainloop()\n",
+    "'''"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "vocal-omega",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "growing-childhood",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "'''\n",
+    "4--->what is used To create a label under parent window?\n",
+    "ans->label = Label(window, text = \"Welcome to Python\")\n",
+    "'''"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "considered-hacker",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "eight-export",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "'''\n",
+    "5--->what is used To create a button under parent window with command processButton?\n",
+    "ans->Button(window, text = \"OK\", command = processButton)\n",
+    "'''"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "equivalent-graph",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "lucky-statistics",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "'''\n",
+    "6--->How do you create a GUI component for displaying multiple-lines of text?\n",
+    "ans->Use Message\n",
+    "'''"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "manufactured-helping",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "stuck-shock",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "'''\n",
+    "7--->How do you draw a rectangle centered at 100, 100 with width 100 and height 100 on canvas?\n",
+    "ans->canvas.create_rectangle(100 - 50, 100 - 50, 100 + 50, 100 + 50)\n",
+    "'''"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "charitable-breast",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "binary-welcome",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "'''\n",
+    "8--->How do you draw a red line from 100, 100 to 400, 500?\n",
+    "ans->canvas.create_line(100, 100, 400, 500, fill = \"red\")\n",
+    "'''"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "short-survey",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "parallel-halloween",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "'''\n",
+    "9--->Assume v1 = IntVar(), how do you set a new value 5 to v1.\n",
+    "ans->v1.set(5)\n",
+    "'''"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "laughing-index",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "sexual-northern",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "'''\n",
+    "10--->what is used To add a menubar?\n",
+    "ans->window.config(menu = menubar)\n",
+    "'''"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "center-victim",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "advance-richards",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "'''\n",
+    "11--->what is used to display a popup menu?\n",
+    "ans->menu.post(300, 300)\n",
+    "'''"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "ceramic-applicant",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "suspected-anxiety",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "'''\n",
+    "12--->what are property The mouse event object has ?\n",
+    "ans->x,y,widget\n",
+    "'''"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.9"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
