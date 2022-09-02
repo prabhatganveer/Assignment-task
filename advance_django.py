1ed77ff8265132008473f74805247350236187c2
Prabhat <92990405+prabhatganveer@users.noreply.github.com>
2022-09-02 16:11:33 +0530

    Create advance_django.py

---
+++ advance_django.py
@@ -0,0 +1,258 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "fifth-relief",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "'''\n",
+    "1--->Why Django should be used for web-development?Explain how you can create a project in Django?\n",
+    "ans->Django is a high-level Python web framework that enables rapid development of secure and maintainable websites. \n",
+    "     Built by experienced developers, Django takes care of much of the hassle of web development, so you can focus on writing \n",
+    "     your app without needing to reinvent the wheel.\n",
+    "'''"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "super-sweden",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "crude-military",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "'''\n",
+    "2--->how to check installed version of django?\n",
+    "ans->Once you have developed an application, then you can check version directly using the following. Simply type python -m \n",
+    "     django --version or type pip freeze to see all the versions of installed modules including Django.\n",
+    "'''"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "institutional-belize",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "corporate-proof",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "'''\n",
+    "3--->Mention what command line can be used to load data into Django?\n",
+    "ans->To load data into Django you have to use the command line Django-admin.py loaddata. The command line will searches the \n",
+    "     data and loads the contents of the named fixtures into the database.\n",
+    "'''"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "unavailable-humor",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "current-weather",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "'''\n",
+    "4--->Explain what does django-admin.py makemessages command is used for?\n",
+    "ans->This command line executes over the entire source tree of the current directory and abstracts all the strings marked for \n",
+    "     translation. It makes a message file in the locale directory.\n",
+    "'''"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "periodic-builder",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "parallel-happiness",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "'''\n",
+    "5--->what is .Django URLs?make program to create django urls\n",
+    "ans->In Django, we use something called URLconf (URL configuration). URLconf is a set of patterns that Django will try to\n",
+    "     match the requested URL to find the correct view.\n",
+    "'''"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "animated-eligibility",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "legal-roman",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "'''\n",
+    "6--->What is a QuerySet? Write program to create a new Post object in database:\n",
+    "ans->A Queryset is simply a list of objects from the Django Models. It can be used to query data as Create, Filter, Update,\n",
+    "     Order etc. Queryset can be written in Django Shell or in views.py.\n",
+    "'''"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "subsequent-creativity",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "undefined-preliminary",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "'''\n",
+    "7--->Mention what command line can be used to load data into Django?\n",
+    "ans->To load data into Django you have to use the command line Django-admin.py loaddata. The command line will searches the \n",
+    "     data and loads the contents of the named fixtures into the database.\n",
+    "'''"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "published-dining",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "corresponding-adobe",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "'''\n",
+    "8--->Explain what does django-admin.py makemessages command is used for?\n",
+    "ans->This command line executes over the entire source tree of the current directory and abstracts all the strings marked for \n",
+    "     translation. It makes a message file in the locale directory.\n",
+    "\n",
+    "'''"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "negative-antenna",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "collective-lawrence",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "'''\n",
+    "9--->Write django appliocation to demonstarte following things.\n",
+    "    1:)Need to use bootstrap template\n",
+    "    2)There will be 2 Modules(User,Admin)\n",
+    "    3)User have to register to site using (id,Firstname,lastname,password,Email,Mobile,city,\n",
+    "    gender,profile pic).data should store in database\n",
+    "    3.1)Email should send to its registered email id With message (Thanks for Registration)\n",
+    "    3.2)Registered user can login using email id and password and rediredted to homepage\n",
+    "    otherwise error message to be shown on same page\n",
+    "    3.3)All field should validated with required filed and mobile number should have\n",
+    "    10 digit and email id should be on proper format\n",
+    "    3.4)User can edit all its detail\n",
+    "ans->\n",
+    "'''"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "rural-morris",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "engaging-synthesis",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "'''\n",
+    "10--->A1.Make Django application to demontsrate following things\n",
+    "        1)There will be 2 modules(Admin,Product manager)\n",
+    "        2)Admin can add product name(ex.Product id and product name) ex.(1,Samsung) , \n",
+    "        (2,Apple)..etc Data should store in Product_mst table with product id as primary key\n",
+    "        3)Admin can add product sub_Category details Like (Product price,product image, \n",
+    "        product model,product Ram) data should store in Product_sub_cat table\n",
+    "        4)Admin can get product name as foreign key from product_mst table in \n",
+    "        product_sub_category_details page\n",
+    "        5)Admin can view,update and delete all registered details of product\n",
+    "        6)product manager can search product on searchbar and get all details about product\n",
+    "        \n",
+    "----->A2.Deploy proiect on github using python command\n",
+    "ans->\n",
+    "'''"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.9"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
